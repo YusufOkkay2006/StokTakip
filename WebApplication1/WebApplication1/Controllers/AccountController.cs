d12a3b81d2edec49be603dbba25088b15ee73a4d
YusufOkkay2006 <117654708+YusufOkkay2006@users.noreply.github.com>
2022-11-18 12:16:59 +0300

    Stok Takip

---
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // GET: Account
+        stoktakipEntities1 db = new stoktakipEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+        
+        [HttpPost]
+        public ActionResult Login(Kullanıcı P)
+
+        {
+            var bilgiler = db.Kullanıcı.FirstOrDefault(x => x.Email==P.Email && x.Şifre==P.Şifre);
+            if (bilgiler!=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Email , false);
+                Session["Mail"]=bilgiler.Email.ToString();
+                Session["Ad"]=bilgiler.Ad.ToString();
+                Session["Soyad"]=bilgiler.Soyad.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.hata="Kullanıcı Adı veya şifre yanlış";
+            }
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Kullanıcı data)
+        {
+            db.Kullanıcı.Add(data);
+            data.Rol ="U";
+            db.SaveChanges();
+           return RedirectToAction("Login", "Account");
+            
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/UrunController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UrunController : Controller
+    {
+
+        // GET: Urun
+        stoktakipEntities1 db = new stoktakipEntities1();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var list = db.Urun.ToList();
+            return View(list);
+
+        }
+
+        public ActionResult Ekle()
+        {
+            List<SelectListItem> deger1 = (from x in db.Kategori.ToList()
+                                           select new SelectListItem
+                                           { 
+                                           Text=x.Ad,
+                                        Value = x.Id.ToString()
+                                         }).ToList();
+            ViewBag.ktgr = deger1;
+            return View();
+ 
+        }
+         [HttpPost]
+         public ActionResult Ekle(Urun Data , HttpPostedFile File)
+        {
+            string path = Path.Combine("~/Content/Image"+File.FileName);
+            File.SaveAs(Server.MapPath(path));
+            Data.Resim = File.FileName.ToString();
+            db.Urun.Add(Data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+        public ActionResult Sil(int id)
+        {
+            var urun = db.Urun.Where(x => x.İd == id).FirstOrDefault();
+            db.Urun.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
